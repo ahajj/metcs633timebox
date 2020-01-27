03de85cced5753e0eaffd7153e593f915f3fd1ec
ahajj <andrew.hajj@gmail.com>
2020-01-26 22:54:30 -0500

    Added functionality to connect to Google Calendar
    Auto connects to Google and then a button to sign in/out

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,15 +1,42 @@
+//Authors: Andrew Hajj
+
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"../model/formatter"
-], function(Controller, formatter) {
+	"../model/formatter",
+	'../services/GoogleCalendarService'
+], function(Controller, formatter, GoogleCalendarService) {
 	"use strict";
+
+      var signedInGoogle = false;
 
 	return Controller.extend("com.metcs633.controller.App", {
 
 		formatter: formatter,
+		GoogleCalendarService: GoogleCalendarService,
 
+// Connect to Google api by default to grab the calendar
 		onInit: function () {
+			var configLabel = this.getView().byId("configLabel");
+			configLabel.setText("Connecting to Google...");
+			GoogleCalendarService.connectToGoogle(configLabel);
+		},
 
-		}
+
+      onSignInOutGooglePress:function(event) {
+      		if(signedInGoogle)
+      		{
+      			GoogleCalendarService.signOut();
+      			this.getView().byId("signButton").setText("Sign Into Google");
+      			this.getView().byId("configLabel").setText("Connected to Google! Now click Sign Into Google to login.");
+      		}
+      		else{
+      			GoogleCalendarService.signIn();
+      			this.getView().byId("signButton").setText("Sign Out of Google");
+      			this.getView().byId("configLabel").setText("Connected to Google! Now click Sign Out of Google to logout.");
+      		}
+      		signedInGoogle = !signedInGoogle;
+      },
+
+
 	});
 });
---
+++ webapp/services/GoogleCalendarService.js
@@ -0,0 +1,56 @@
+// Author: Andrew Hajj
+// Purpose: functions to get calendar info from google
+
+sap.ui.define("com/metcs633/services/GoogleCalendarService", [
+], function() {
+	"use strict";
+
+	var Utils = {};
+
+	      // Client ID and API key from the Developer Console
+      var CLIENT_ID = '782211928709-49gkv11kmh5mji6m79rs9df57ba3u6pv.apps.googleusercontent.com';
+      var API_KEY = 'AIzaSyBrQSmVrwuA4-pR6jO5UdfizdTRzqDQnn0';
+
+      // Array of API discovery doc URLs for APIs used by the quickstart
+      var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+      // Authorization scopes required by the API; multiple scopes can be
+      // included, separated by spaces.
+      var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
+	Utils.signIn = function (event) {
+        gapi.auth2.getAuthInstance().signIn();
+      };
+
+	 // signout of google
+	 Utils.signOut = function(event) {
+	        gapi.auth2.getAuthInstance().signOut();
+	      };
+
+	  		//  call to connect to Google
+		Utils.connectToGoogle =function(label) {
+			gapi.load('client:auth2', this.initiateClient(label));
+
+		};
+
+		Utils.initiateClient = function(label) {
+
+			var me = this;
+
+			gapi.client.init({
+	          apiKey: API_KEY,
+	          clientId: CLIENT_ID,
+	          discoveryDocs: DISCOVERY_DOCS,
+	          scope: SCOPES
+	        }).then(function () {
+	          // Listen for sign-in state changes.
+	          gapi.auth2.getAuthInstance().isSignedIn.listen(label.setText("Connected to Google! Now click Sign In."));
+
+
+	        }, function(error) {
+	          console.log(JSON.stringify(error, null, 2));
+	        });
+		};
+
+      return Utils;
+}, true /* bExport */ );
