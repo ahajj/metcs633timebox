114cae29dac4ce112e52dbb000238a71d221dc5e
Christopher Woodland <christopher.woodland@pearson.com>
2020-02-06 23:03:19 -0500

    add getCategories add getCalendars add getCalendarById

--- webapp/services/O365CalendarService.js
+++ webapp/services/O365CalendarService.js
@@ -5,12 +5,12 @@ sap.ui.define([],
 		"use strict";
 
 		var Utils = {};
+		var oMsalClient = null;
 
 		const APP_ID = "f43b3bbb-f2aa-4134-9518-e0a190584c6f";
 		const APP_PASSWORD = "l42qTX74yi@xDfgeuG_TOxWrTXJMfx=/";
 		const APP_SCOPES = ['Calendars.Read', 'Calendars.ReadWrite'];
 		const MS_GRAPH_BASE = "https://graph.microsoft.com"
-
 		const config = {
 			msalConfig: {
 				auth: {
@@ -26,6 +26,34 @@ sap.ui.define([],
 				scopes: APP_SCOPES
 			}
 		};
+		//#region Helpers
+		function handleAuthError(error) {
+			console.log(error);
+		}
+
+		function getMSGraphClient() {
+			if (!oMsalClient) {
+				return new Msal.UserAgentApplication(config.msalConfig);
+			}
+			return oMsalClient;
+		}
+		//#endregion
+		Utils.initiateClient = (signout = false) => {
+			let oMsalClient = getMSGraphClient();
+			if (signout) {
+				oMsalClient.logout();
+				return true;
+			}
+			//check if the user is already signed in
+			if (!oMsalClient.getAccount()) {
+				oMsalClient.loginPopup(config.scopeConfig).then((res) => {
+					return res.idToken;
+				}).catch((error) => {
+					handleAuthError(error);
+				});
+			}
+			return oMsalClient.getAccount();
+		};
 
 		Utils.signIn = function (event) {
 			var ret = this.initiateClient();
@@ -34,6 +62,8 @@ sap.ui.define([],
 				console.log('O365 token: ', ret.idToken)
 				event.getView().byId("signButtonO365").setText("Sign Out of O365");
 				event.getView().byId("configLabel").setText("Connected to O365!");
+				this.getCalendars();
+				this.getCategories();
 			}
 		};
 
@@ -45,27 +75,100 @@ sap.ui.define([],
 			}
 		};
 
-		function handleAuthError(error) {
-			console.log(error);
+		//#region Calendars
+		Utils.getCalendars = function () {
+			getMSGraphClient().acquireTokenSilent(config.scopeConfig).then((token) => {
+				$.ajax({
+					headers: {
+						"Authorization": "Bearer " + token.accessToken
+					},
+					url: `${config.graphBaseEndpoint}/me/calendars`,
+					type: "GET"
+				}).then((res) => {
+					console.log(`getCalendars`, res);
+				}).fail((error) => {
+					handleAuthError(error);
+				});
+			});
 		}
 
-		Utils.initiateClient = (signout = false) => {
-			this.oMsalClient = new Msal.UserAgentApplication(config.msalConfig);
-			if (signout) {
-				this.oMsalClient.logout();
-				return true;
-			}
-			//check if the user is already signed in
-			if (!this.oMsalClient.getAccount()) {
-				this.oMsalClient.loginPopup(config.scopeConfig).then((res) => {
-					return res.idToken;
-				}).catch((error) => {
+		Utils.getCalendarById = function (id) {
+			getMSGraphClient().acquireTokenSilent(config.scopeConfig).then((token) => {
+				$.ajax({
+					headers: {
+						"Authorization": "Bearer " + token.accessToken
+					},
+					url: `${config.graphBaseEndpoint}/me/calendars/${id}`,
+					type: "GET"
+				}).then((res) => {
+					console.log(`getCalendars`, res);
+				}).fail((error) => {
 					handleAuthError(error);
-				});;
-			}
-			return this.oMsalClient.getAccount();
-		};
+				});
+			});
+		}
+		//#endregion
 
+
+
+		//#region Categories
+		function getStartDate(startDate, endDate) {
+			let start, end = new Date();
+			if (startDate && endDate) {
+				start = new Date(new Date(startDate).setHours(0, 0, 0));
+				end = new Date(new Date(endDate).setHours(0, 0, 0));
+			} else {
+				end = new Date(new Date().setHours(0, 0, 0));
+				start = new Date(new Date(2015, 3, 2).setHours(0, 0, 0));
+			}
+			return { start, end };
+		}
+
+		Utils.getCategories = function (startDate, endDate, calendarId = '') {
+			let start, end = new Date();
+
+			//start and end date format 2020-01-31
+			let dates = getStartDate(startDate, endDate);
+			start = dates.start;
+			end = dates.end;
+
+			console.log('start date:', start);
+			console.log('end date:', end);
+			
+			var apiUrl = '';
+			if (calendarId && calendarId.length > 0) {
+				apiUrl = `/me/calendars/${calendarId}/calendarView?startDateTime=${start.toISOString()}&endDateTime=${end.toISOString()}`;
+			} else {
+				apiUrl = `/me/calendar/calendarView?startDateTime=${start.toISOString()}&endDateTime=${end.toISOString()}`;
+			}
+			getMSGraphClient().acquireTokenSilent(config.scopeConfig).then((token) => {
+				$.ajax({
+					headers: {
+						"Authorization": "Bearer " + token.accessToken
+					},
+					url: `${config.graphBaseEndpoint}${apiUrl}`,
+					type: "GET"
+				}).then((categories) => {
+					console.log(`getCategories`, categories);
+
+					let retCategories = [];
+					if (categories && categories.value) {
+						categories.value.forEach(element => {
+							retCategories = retCategories.concat(element.categories);
+						});
+					}
+					categories = JSON.stringify([...new Set(retCategories)]);
+					console.log('getCategories result:', categories);
+
+				}).fail((error) => {
+					handleAuthError(error);
+				});
+			});
+		}
+		//#endregion
+
+		Utils.getListOfEventsFromCalendarInDateRange = function (calendar, startDate, endDate, controller) {
+		}
 
 		return Utils;
 	}, true /* bExport */);
