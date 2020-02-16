a9e8476f90f3e51b647ad9541f725d079653063e
Christopher Woodland <christopher.woodland@pearson.com>
2020-02-15 23:23:12 -0500

    Add getDateString to googlecalendarservice.js

--- webapp/services/GoogleCalendarService.js
+++ webapp/services/GoogleCalendarService.js
@@ -70,6 +70,18 @@ sap.ui.define('com/metcs633/services/GoogleCalendarService', [
 		]
 	};
 
+	function getDateString(dateIn) {
+		dateIn = new Date(dateIn);
+		var dateString =
+			dateIn.getFullYear() + "/" +
+			("0" + (dateIn.getMonth() + 1)).slice(-2) + "/" +
+			("0" + dateIn.getDate()).slice(-2) + " " +
+			("0" + dateIn.getHours()).slice(-2) + ":" +
+			("0" + dateIn.getMinutes()).slice(-2) + ":" +
+			("0" + dateIn.getSeconds()).slice(-2);
+		return dateString;
+	};
+
 	// Function to authenticate the user and choose the Google Account
 	Utils.signIn = function (event) {
 		gapi.auth2.getAuthInstance().signIn();
@@ -251,7 +263,6 @@ sap.ui.define('com/metcs633/services/GoogleCalendarService', [
 			return listOfCategories;
 		},
 
-
 		Utils.parseListOfEvents = function (events, callback) {
 			var parsedEvents = [];
 
@@ -277,7 +288,8 @@ sap.ui.define('com/metcs633/services/GoogleCalendarService', [
 
 				// now get the time difference in hours between the start time and the end time
 				parsedEvent.time = Math.ceil(Math.abs(parsedEvent.endTime - parsedEvent.startTime) / (1000 * 60 * 60));
-
+				parsedEvent.startTimeString = getDateString(startTime);
+				parsedEvent.endTimeString = getDateString(endTime);
 				// if time is NaN that is problem
 				if (isNaN(parsedEvent.time)) {
 					console.log(isNan(event));
